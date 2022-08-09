b784d7e6cf1aeb225db95bd5ce61ef6b938a54da
Xiangrui Zong <zxr@tju.edu.cn>
2022-08-09 14:18:10 +0800

    complete exercise 8.2
    complete exercise 8.2

---
+++ chapter9/9.1/exercise2.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+
+def read_txt(path: str, separate: str) -> [[str]]:
+    """
+    读取文本至二维列表\n
+    :param path: 路径
+    :param separate: 分隔符
+    :return: 二维列表
+    """
+    result = []
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f:
+            result.append(line.strip().split(separate))
+    return result
+
+
+if __name__ == '__main__':
+    # 1
+    df5 = pd.read_csv("./data1.txt", delimiter='\t', names=["header"])
+    print(df5)
+    # 2
+    print(read_txt("./data2.txt", separate="/$/"))
