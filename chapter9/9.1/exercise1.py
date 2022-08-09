3e654e7923caa1ec8e43fb3df021fd6f7c1d92ba
Xiangrui Zong <zxr@tju.edu.cn>
2022-08-09 10:53:54 +0800

    complete exercise 8.1.(1)
    complete exercise 8.1.(1)

--- chapter9/9.1/exercise1.py
+++ chapter9/9.1/exercise1.py
@@ -3,6 +3,11 @@ import pandas as pd
 
 
 if __name__ == '__main__':
+    # 1
     df1 = pd.read_csv("./data1.csv")
     print(df1)
+    # 2
+    df2 = pd.read_csv("./data2.csv", header=None)
+    df2.columns = df1.columns
+    print(df2)
 
