bbfd4c6cbc77e0083b6fc8d735d6f6c507081af9
Xiangrui Zong <zxr@tju.edu.cn>
2022-08-09 15:32:43 +0800

    complete exercise 8.4
    complete exercise 8.4

---
+++ chapter9/9.1/exercise4.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    # 1
+    with open("./result4_1.txt", "w", encoding="utf-8") as f:
+        for i in range(1, 101):
+            f.write(str(i))
+            if i == 100:
+                break
+            f.write("$")
+    # 2
+    with open("./result4_2.txt", "w", encoding="utf-8") as f:
+        for i in range(0, 101):
+            f.write("%d,%d,%d" % (i, i + 100, i * (i + 100)))
+            if i == 100:
+                break
+            f.write("\n")
