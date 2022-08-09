a64550ac0f660fc0dd48c80f5d249f245f0d52ed
zong.xiangrui <zxr@tju.edu.cn>
2022-08-09 22:14:23 +0800

    complete exercise 8.7
    complete exercise 8.7

---
+++ chapter9/9.2/exercise7.py
@@ -0,0 +1,15 @@
+# -*- coding: UTF-8 -*-
+import pandas as pd
+
+
+if __name__ == '__main__':
+    # 1
+    df = pd.read_csv("./example.csv")
+    df1 = df.drop_duplicates()
+    print(df1)
+    # 2
+    df2 = df.drop_duplicates(subset=["Salary"])
+    print(df2)
+    # 3
+    df3 = df.drop_duplicates(subset=["ID", "Salary", "Age"])
+    print(df3)
