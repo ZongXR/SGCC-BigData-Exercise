21a745602f70b14b6ec9582a3f16889b34c7cade
zong.xiangrui <zxr@tju.edu.cn>
2025-08-15 16:02:43 +0800

    补充chapter10第1题的题目
    补充chapter10第1题的题目

--- chapter10/01.py
+++ chapter10/01.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
+"""
+在sklearn.datasets中可以自行加载load_breast_cancer数据，具体数据如下：
+cancer = load_breast_cancer()
+X = cancer.data
+y = cancer.target
+该题直接调用模型不需要对样本进行切分，参数设置为max_depth=i，i为range(5, 10)，请采用决策树做分类输出不同max_depth下的model.score(X, y)
+"""
 from sklearn.datasets import load_breast_cancer
 from sklearn.tree import DecisionTreeClassifier
 
